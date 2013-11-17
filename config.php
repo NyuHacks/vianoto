bcb352a8531eb26622339989995267fc4f40e9b3
andrei-savin <glork2006@gmail.com>
2013-11-16 21:41:49 -0500

    test3

--- config.php
+++ config.php
@@ -1,14 +1,8 @@
 <?php
 session_start();
 
-$url = parse_url('mysql://b53be1921dd5ee:8af16c38@us-cdbr-east-04.cleardb.com/heroku_677bd37e289b91b?');
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"],1);
-
-mysql_connect($server, $username, $password)or die('MySQL is being nasty');
-mysql_select_db($db)or die('MySQL hates you');
+mysql_connect('us-cdbr-east-04.cleardb.com', 'b53be1921dd5ee', '8af16c38')or die('MySQL is being nasty');
+mysql_select_db('heroku_677bd37e289b91b')or die('MySQL hates you');
 
 // If user is logged in, retrieve user information
 if(!empty($_SESSION['U']))
