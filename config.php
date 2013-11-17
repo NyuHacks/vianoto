4311708ee32f2df101ea7a98fab8a2d912a0619d
andrei-savin <glork2006@gmail.com>
2013-11-16 21:25:43 -0500

    MySQL config

--- config.php
+++ config.php
@@ -1,14 +1,19 @@
 <?php
 session_start();
 
-mysql_connect('','','') or die('CANNOT CONNECT TO DATABSE');
-mysql_select_db('')or die('CANNOT SELECT DATABASE');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"],1);
 
+mysql_connect($server, $username, $password)or die('MySQL is being nasty');
+mysql_select_db($db)or die('MySQL hates you');
 
 // If user is logged in, retrieve user information
-if(empty($_SESSION['U'])) {
+if(empty($_SESSION['U']))
 	$U = $_SESSION['u'];
-}
+else $U = '';
 
 
 require 'page.php';
